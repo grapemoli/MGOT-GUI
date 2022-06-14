80c3bd4838b12eaeeaed374964a41bd83f6ce43e
grapemoli <wambamgara@gmail.com>
2022-06-14 19:22:16 -0400

    mess around with swing

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("hello");
+        JFrame frame = new JFrame("poop");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        JButton button = new JButton("Press");
+        JButton button2 = new JButton("mine");
+        frame.getContentPane().add(button); // Adds Button to content pane of frame
+        frame.getContentPane().add(button2);
+        frame.setVisible(true);
 
     }
-
 }
+
+
